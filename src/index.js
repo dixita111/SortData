4b81568ab53fc2aa7702c32dea5fa8c391af469e
dixita111 <dixitakavani.dreamspot@gmail.com>
2024-03-07 10:37:41 +0530

    sortdata

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import UserTable from "./search";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <br />
-    <br />
-    <UserTable />
-  </React.StrictMode>,
+
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/table.js
+++ src/table.js
@@ -1,28 +1,33 @@
 import "./table.css";
 
 function Sort({ demo }) {
-  return (
-    <>
-      <table>
-        <thead>
-          <tr>
-            <th>FirstName</th>
-            <th>LastName</th>
-          </tr>
-        </thead>
-        <tbody>
-          {demo?.map((item, index) => {
-            return (
-              <tr>
-                <td>{item.fnm}</td>
-                <td>{item.lnm}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </>
-  );
+    return (
+        <>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>SchoolName</th>
+                        <th>Course</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {demo?.map((item, index) => {
+                        return (
+                            <tr>
+                                <td>{item.rno}</td>
+                                <td>{item.fname}</td>
+                                <td>{item.school}</td>
+                                <td>{item.course}</td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </>
+    );
 }
 
 export default Sort;
+
